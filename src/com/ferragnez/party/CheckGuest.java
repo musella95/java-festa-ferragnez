f8b0093bd72d90b7da7ba5211c6df9f08a062485
musella95 <musellamatteoo@hotmail.com>
2023-11-30 14:59:39 +0100

    work done

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,2 +1,42 @@
-package com.ferragnez.party;public class CheckGuest {
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        // creo un array di stringhe e gli assegno il suo valore
+        String [] partyGuest = {
+            "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                    "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"
+        };
+        // avvio lo scanner
+        Scanner scan = new Scanner(System.in);
+        // chiedo all'utente come si chiama
+        System.out.println("Come ti chiami?");
+        //inserisco i dati forniti dall'utente
+        String nameUser = scan.nextLine();
+        boolean found = false;
+        for (int i = 0; i < partyGuest.length && !found; i++) {
+            if (nameUser==partyGuest[i]){
+                found = true;
+            }
+
+        }
+        if(!found){ // come dire found == false
+            System.out.println("SEI SALVO NON FAI PARTE DI LORO");
+        } else {
+            System.out.println("BEVENUTO NEL DISAGIO");
+        }
+
+
+
+
+
+
+
+
+
+
+                scan.close();
+    }
 }
