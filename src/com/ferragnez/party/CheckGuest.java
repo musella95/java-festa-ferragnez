77cc6cf489ef668d30e6b898c3c411cc70794df0
musella95 <musellamatteoo@hotmail.com>
2023-11-30 15:08:30 +0100

    adjust

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -17,15 +17,15 @@ public class CheckGuest {
         String nameUser = scan.nextLine();
         boolean found = false;
         for (int i = 0; i < partyGuest.length && !found; i++) {
-            if (nameUser==partyGuest[i]){
+            if (nameUser.equals(partyGuest[i])){
                 found = true;
             }
 
         }
-        if(!found){ // come dire found == false
-            System.out.println("SEI SALVO NON FAI PARTE DI LORO");
+        if(found){ // come dire found == false
+            System.out.println(" BEVENUTO NEL DISAGIO");
         } else {
-            System.out.println("BEVENUTO NEL DISAGIO");
+            System.out.println("SEI SALVO NON FAI PARTE DI LORO");
         }
 
 
